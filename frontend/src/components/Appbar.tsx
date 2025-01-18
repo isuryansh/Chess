5e5f0a7d3c295409d992dd268bf84f236edbb7ce
Suryansh <suryanshvaish1@gmail.com>
2025-01-19 01:23:50 +0530

    updating frontend

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -1,6 +1,6 @@
 export const Appbar = () => {
   return (
-    <div className="flex justify-start text-white text-2xl font-bold pl-16 pt-6 pb-2.5  ring-1 ring-white">
+    <div className="flex justify-start text-white text-2xl font-bold pl-16 pt-6 pb-2.5  ring-2 ring-white">
       Chess.com
     </div>
   );
--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { Appbar } from "../components/Appbar";
 
 export const Landing = () => {
@@ -11,14 +12,14 @@ export const Landing = () => {
           <div className="w-4/12 mt-20 mr-12 ml-32">
             <img src={imageUrl} />
           </div>
-          <div>
-            <button className="bg-green-500 text-white text-3xl font-semibold px-4 py-1.5 rounded-lg mt-72 mr-32 ml-20  hover:bg-green-600 cursor-pointer">
+          <Link to="/game">
+            <button className="bg-green-500 text-white text-3xl font-semibold px-6 py-2 ring-2 ring-white rounded-lg mt-72 mr-32 ml-20  hover:bg-green-600 cursor-pointer">
               Play online
-              <div className="text-xs font-normal pt-1 text-white">
+              <div className="text-xs font-normal pt-1.5 text-white">
                 Play with some random player
               </div>
             </button>
-          </div>
+          </Link>
         </div>
       </div>
     </>
